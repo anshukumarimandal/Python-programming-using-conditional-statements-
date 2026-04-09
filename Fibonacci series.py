1883258700edd96e11e250150f5659f0faca7a57
Anshukumari Mandal <anshukumarimandal09@gmail.com>
2026-04-09 16:06:33 +0530

    Add files via upload

---
+++ Fibonacci series.py
@@ -0,0 +1,12 @@
+# Fibonacci series up to n terms
+n = int(input("Enter number of terms: "))
+a, b = 0, 1
+if n <= 0:
+    print("Please enter a positive number")
+elif n == 1:
+    print("Fibonacci series:", a)
+else:
+    print("Fibonacci series:")
+    for i in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
